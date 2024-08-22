c0f55e3b28289e837cc557aa1dc237061d436358
gantulu <117321632+gantulu@users.noreply.github.com>
2024-08-22 23:18:26 +0700

    12312

--- apijs.js
+++ apijs.js
@@ -1,1 +1,1 @@
-//api.tokopay.id/v1/order?merchant=[M240509URZTI530&secret=5bc43dda41b1893283f01d3e2c5ba567076fbe7ac80840576a701fdc9dfa23ca&ref_id=DEV-92018&nominal=12000&metode=BRIVA
+//api.tokopay.id/v1/order?merchant=M240509URZTI530&secret=5bc43dda41b1893283f01d3e2c5ba567076fbe7ac80840576a701fdc9dfa23ca&ref_id=DEV-92018&nominal=12000&metode=BRIVA
